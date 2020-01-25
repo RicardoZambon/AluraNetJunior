6d586baa9e5c95c2595ebe6ec62fdf0cac7dc392
Ricardo Zambon <ricardo.zambon@hotmail.com>
2020-01-25 18:42:40 -0300

    Organizei os Models, estavam todos agrupados em um único arquivo .cs, desta forma melhora a visualização e manutenção.

---
+++ CasaDoCodigo/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CasaDoCodigo.Models
+{
+    public class Categoria : BaseModel
+    {
+        [Required]
+        public string Nome { get; set; }
+
+        public Categoria()
+        {
+
+        }
+        public Categoria(string nome)
+        {
+            this.Nome = nome;
+        }
+    }
+}
---
+++ CasaDoCodigo/Models/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CasaDoCodigo.Models
+{
+    public class Produto : BaseModel
+    {
+        [Required]
+        public string Codigo { get; private set; }
+        [Required]
+        public string Nome { get; private set; }
+        [Required]
+        public decimal Preco { get; private set; }
+        [Required]
+        public virtual Categoria Categoria { get; private set; }
+
+        public Produto()
+        {
+
+        }
+        public Produto(string codigo, string nome, decimal preco, Categoria categoria)
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Categoria = categoria;
+        }
+    }
+}
